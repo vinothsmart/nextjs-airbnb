5660ffeebbe16774ac44d94a77da2d804a60eeaa
vinothsmart <s.vinothkanna90@gmail.com>
2026-04-17 11:54:11 +0530

    enhance HomePage component by adding navigation link to About Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div className="text-7xl">HomePage</div>;
+  return (
+    <>
+      <div className="text-7xl">HomePage</div>
+      <Link href="/about" className="text-xl text-blue-500 inline-block mt-8">
+        Go to About Page
+      </Link>
+    </>
+  );
 };
 export default HomePage;
