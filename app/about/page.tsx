6b3b183ee6935db13aefadef88d93d9fba711562
vinothsmart <s.vinothkanna90@gmail.com>
2026-04-17 11:49:33 +0530

    add AboutPage and InfoPage components with basic structure

---
+++ app/about/page.tsx
@@ -0,0 +1,4 @@
+const AboutPage = () => {
+  return <div className="text-7xl">AboutPage</div>;
+};
+export default AboutPage;
---
+++ app/info/page.tsx
@@ -0,0 +1,4 @@
+const InfoPage = () => {
+  return <div className="text-7xl">InfoPage</div>;
+};
+export default InfoPage;
