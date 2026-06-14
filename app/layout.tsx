31ec616a0846bcf28518deb0bc8ba14c0d6aca82
vinothsmart <s.vinothkanna90@gmail.com>
2026-06-14 13:15:39 +0530

    Multiple fonts using

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import NavBar from "@/components/Navbar";
 import "./globals.css";
+import { Inter, Inconsolata, Roboto } from "next/font/google";
 
+const inter = Inter({ subsets: ["latin"] });
+const inconsolata = Inconsolata({ subsets: ["latin"] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["400"] });
 export default function RootLayout({
   children,
 }: {
@@ -8,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${roboto.className} bg-gray-100 min-h-screen`}>
         <NavBar />
         <main className="max-w-3xl mx-auto py-10">{children}</main>
       </body>
