d02309071e912e5bd7f2bf87eb3a2ae0e95443ac
vinothsmart <s.vinothkanna90@gmail.com>
2026-07-20 15:53:47 +0530

    Default font added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import NavBar from "@/components/Navbar";
 import "./globals.css";
-import { Inter, Inconsolata, Roboto } from "next/font/google";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-const inconsolata = Inconsolata({ subsets: ["latin"] });
-const roboto = Roboto({ subsets: ["latin"], weight: "400" });
 
 export default function RootLayout({
   children,
@@ -13,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${roboto.className} bg-gray-100 min-h-screen`}>
+      <body className={`${inter.className} bg-gray-100 min-h-screen`}>
         <NavBar />
         <main className="max-w-3xl mx-auto py-10">{children}</main>
       </body>
