93b0cec5a81b9ccbca8023d690d0f064d32eec9c
vinothsmart <s.vinothkanna90@gmail.com>
2026-07-20 16:27:47 +0530

    Using form comp

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -1,24 +1,11 @@
-"use client";
-
-import { useState, useCallback } from "react";
+import Counter from "@/components/Counter";
 
 const CounterPage = () => {
-  const [count, setCount] = useState(0);
-
-  const handleIncrement = useCallback(() => {
-    setCount((prevCount) => prevCount + 1);
-  }, []);
-
   return (
-    <div className="flex flex-col items-center justify-center w-[100px]">
-      <p className="text-5xl font-bold">{count}</p>
-      <button
-        className="bg-blue-500 rounded text-white px-4 py-2 mt-4"
-        onClick={handleIncrement}
-      >
-        Increment
-      </button>
-    </div>
+    <section>
+      <h1 className="text-6xl mb-16">Page Content</h1>
+      <Counter />
+    </section>
   );
 };
 
