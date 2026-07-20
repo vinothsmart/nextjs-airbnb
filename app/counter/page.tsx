7d536b7270e13fd9e23b330517700988d44f582e
vinothsmart <s.vinothkanna90@gmail.com>
2026-07-20 16:23:25 +0530

    Counter page using use client

---
+++ app/counter/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState, useCallback } from "react";
+
+const CounterPage = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = useCallback(() => {
+    setCount((prevCount) => prevCount + 1);
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center w-[100px]">
+      <p className="text-5xl font-bold">{count}</p>
+      <button
+        className="bg-blue-500 rounded text-white px-4 py-2 mt-4"
+        onClick={handleIncrement}
+      >
+        Increment
+      </button>
+    </div>
+  );
+};
+
+export default CounterPage;
